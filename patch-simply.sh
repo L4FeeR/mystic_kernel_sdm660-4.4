e83129d8ef7f4eed8e1b61009cf08f4dcf42baef
L4FeeR <muhamedlafeer837@gmail.com>
2023-05-07 13:48:15 +0000

    Updated the patcher file
    additional fixes

--- patch-simply.sh
+++ patch-simply.sh
@@ -1,15 +1,15 @@
 #patching the source with nethunter patches, in order to work
 echo "  [+] Setting patch files...
-cp nh_patches/* ./
+ls nh_patches/
 echo "  [+] Setting up Done.
 echo "  [+] Patching..."
 echo
-patch -p1 < ath9k:_Add_cast_to_u8_to_FREQ2FBIN_macro.patch
-patch -p1 <fix-ath9k_naming_conflict.patch
-patch -p1 <fix-header.patch
-patch -p1 <fix-rxlock-spin.patch
-patch -p1 <injection.patch
-patch -p1 <rndis-fix.patch
+patch -p1 < nh_patches/ath9k:_Add_cast_to_u8_to_FREQ2FBIN_macro.patch
+patch -p1 < nh_patches/fix-ath9k_naming_conflict.patch
+patch -p1 < nh_patches/fix-header.patch
+patch -p1 < nh_patches/fix-rxlock-spin.patch
+patch -p1 < nh_patches/injection.patch
+patch -p1 < nh_patches/rndis-fix.patch
 echo
 echo "   [+] Patching Done."
 echo
